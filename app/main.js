4c84f95ca9beb1f07332b40f807dcafbab1ab900
Peter Ajtai <peter.ajtai@thinksolid.com>
2015-09-22 17:34:11 -0700

    failed modularization attempt

--- app/main.js
+++ app/main.js
@@ -2,26 +2,27 @@
 
 var wireframe = require('wireframe-ui/client');
 
+
 init();
 
 function init() {
     var appState = {
-        title : 'Project Title',
-        nav_items : [
-            'Home',
-            'Gallery',
-            'About'
-        ],
-        list1 : [
-            'So much list',
-            'List is so list',
-            'Many words'
-        ],
-        list2 : [
-            'Such list',
-            'List is much list'
-        ]
-    };
+            title : 'Project Title',
+            nav_items : [
+                'Home',
+                'Gallery',
+                'About'
+            ],
+            list1 : [
+                'So much list',
+                'List is so list',
+                'Many words'
+            ],
+            list2 : [
+                'Such list',
+                'List is much list'
+            ]
+        };
 
     wireframe.start(appState);
 }
